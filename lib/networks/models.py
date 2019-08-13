ce90cc827d35c58bf19f1480fc5aceda426b5bfd
TactfulYuu <princehaohao@sjtu.edu.cn>
2019-08-13 12:34:20 -0400

    change activation function to sigmoid

--- lib/networks/models.py
+++ lib/networks/models.py
@@ -253,10 +253,10 @@ class dual_graph_vrd(basenet):
         # compute compatibility scores
         (self.feed(sub_vert_w_input)
              .fc(1, relu=False, reuse=reuse, name='sub_vert_w_fc')
-             .relu(name='sub_vert_score'))
+             .sigmoid(name='sub_vert_score'))
         (self.feed(obj_vert_w_input)
              .fc(1, relu=False, reuse=reuse, name='obj_vert_w_fc')
-             .relu(name='obj_vert_score'))
+             .sigmoid(name='obj_vert_score'))
 
         sub_vert_w = self.get_output('sub_vert_score')
         obj_vert_w = self.get_output('obj_vert_score')
@@ -282,10 +282,10 @@ class dual_graph_vrd(basenet):
         # compute compatibility scores
         (self.feed(out_edge_w_input)
              .fc(1, relu=False, reuse=reuse, name='out_edge_w_fc')
-             .relu(name='out_edge_score'))
+             .sigmoid(name='out_edge_score'))
         (self.feed(in_edge_w_input)
              .fc(1, relu=False, reuse=reuse, name='in_edge_w_fc')
-             .relu(name='in_edge_score'))
+             .sigmoid(name='in_edge_score'))
 
         out_edge_w = self.get_output('out_edge_score')
         in_edge_w = self.get_output('in_edge_score')
